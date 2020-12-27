c46e406b43ccd2cd7d05ca50c551114e9d4618ab
zadykian <zadykian@outlook.com>
2020-12-27 18:31:15 +0300

    *

--- Postgres.Marula.AppHost/Program.cs
+++ Postgres.Marula.AppHost/Program.cs
@@ -2,12 +2,12 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Postgres.Marula.Host
+namespace Postgres.Marula.AppHost
 {
 	internal static class Program
 	{
 		private static async Task Main(string[] args)
-			=> await Microsoft.Extensions.Hosting.Host
+			=> await Host
 				.CreateDefaultBuilder(args)
 				.ConfigureServices((_, services) => services.AddHostedService<Worker>())
 				.Build()
--- Postgres.Marula.AppHost/Worker.cs
+++ Postgres.Marula.AppHost/Worker.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Postgres.Marula.Host
+namespace Postgres.Marula.AppHost
 {
 	/// <inheritdoc />
 	internal class Worker : BackgroundService
