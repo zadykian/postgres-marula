19ce235c85abda80fdd0786a8544d028d7655555
zadykian <zadykian@outlook.com>
2021-02-04 15:49:48 +0300

    solution components

--- Postgres.Marula.DatabaseAccess/Conventions/DefaultNamingConventions.cs
+++
@@ -1,9 +0,0 @@
-namespace Postgres.Marula.DatabaseAccess.Conventions
-{
-	/// <inheritdoc />
-	internal class DefaultNamingConventions : INamingConventions
-	{
-		/// <inheritdoc />
-		string INamingConventions.SystemSchemaName => "pg_marula";
-	}
-}
--- Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/system-schema.sql
+++
@@ -1,3 +0,0 @@
--- execution-order: 0
-
-create schema SYSTEM_SCHEMA_NAME_TO_REPLACE;
