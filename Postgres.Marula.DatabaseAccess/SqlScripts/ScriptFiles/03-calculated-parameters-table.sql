7a19a453ae0962e5ef18e56e91049832c9fc8967
zadykian <zadykian@outlook.com>
2021-02-06 13:33:53 +0300

    *

--- Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/03-calculated-parameters-table.sql
+++ Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/03-calculated-parameters-table.sql
@@ -9,7 +9,7 @@ create table calculated_parameters
 	unit parameter_unit not null
 );
 
-comment on table calculated_parameters       is 'Dictionary of parameters calculated by marula.';
+comment on table  calculated_parameters      is 'Dictionary of parameters calculated by marula.';
 comment on column calculated_parameters.id   is 'Table identifier.';
 comment on column calculated_parameters.name is 'Parameter name. Matches [pg_catalog.pg_settings.name] value.';
 comment on column calculated_parameters.unit is 'Parameter unit.';
