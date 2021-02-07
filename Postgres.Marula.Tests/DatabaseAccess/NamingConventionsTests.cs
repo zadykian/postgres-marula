7202ab952b55613e60bb4745e93e5a09d698dccd
zadykian <zadykian@outlook.com>
2021-02-07 20:29:11 +0300

    naming conventions tests

--- Postgres.Marula.Tests/DatabaseAccess/NamingConventionsTests.cs
+++ Postgres.Marula.Tests/DatabaseAccess/NamingConventionsTests.cs
@@ -17,30 +17,25 @@ namespace Postgres.Marula.Tests.DatabaseAccess
 		/// </summary>
 		[Test]
 		public void SystemSchemaNameHasValidFormatTest()
-		{
-			var namingConventions = GetService<INamingConventions>();
-
-			Regex
-				.IsMatch(namingConventions.SystemSchemaName, @"^[a-z|A-Z|_]{1}[a-z|A-Z|_|\d]{1,62}$")
+			=> GetService<INamingConventions>()
+				.To(namingConventions => Regex.IsMatch(
+					namingConventions.SystemSchemaName,
+					@"^[a-z|A-Z|_]{1}[a-z|A-Z|_|\d]{1,62}$"))
 				.To(hasValidFormat => Assert.IsTrue(
 					hasValidFormat,
 					$"{nameof(INamingConventions.SystemSchemaName)} must be valid postgres object name."));
-		}
 
 		/// <summary>
 		/// System schema name prefix test.
 		/// </summary>
 		[Test]
 		public void SystemSchemaNameDoesNotHaveReservedPrefixTest()
-		{
-			var namingConventions = GetService<INamingConventions>();
-
-			namingConventions
-				.SystemSchemaName
-				.StartsWith("pg_")
+			=> GetService<INamingConventions>()
+				.To(namingConventions => namingConventions
+					.SystemSchemaName
+					.StartsWith("pg_"))
 				.To(hasReservedPrefix => Assert.IsFalse(
 					hasReservedPrefix,
 					$"{nameof(INamingConventions.SystemSchemaName)} can't have prefix 'pg_' which is reserved for postgres internals."));
-		}
 	}
 }
