60b5ac0b15ee60af80544daf501fb6bab459c0ca
zadykian <zadykian@outlook.com>
2021-02-11 16:05:34 +0300

    load assemblies from base directory

--- Postgres.Marula.Infrastructure/SolutionComponents/Factory/DefaultSolutionComponentsFactory.cs
+++ Postgres.Marula.Infrastructure/SolutionComponents/Factory/DefaultSolutionComponentsFactory.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using Postgres.Marula.Infrastructure.Extensions;
 
 namespace Postgres.Marula.Infrastructure.SolutionComponents.Factory
 {
@@ -10,14 +13,23 @@ namespace Postgres.Marula.Infrastructure.SolutionComponents.Factory
 	{
 		/// <inheritdoc />
 		IEnumerable<ISolutionComponent> ISolutionComponentsFactory.CreateAll()
-			=> AppDomain
-				.CurrentDomain
-				.GetAssemblies()
+			=> GetAssembliesToGetComponentsFrom()
 				.SelectMany(assembly => assembly.GetTypes())
 				.Where(ComponentTypePredicate)
 				.Select(Activator.CreateInstance)
 				.Cast<ISolutionComponent>()
 				.ToImmutableArray();
+
+		/// <summary>
+		/// Get assemblies to access solution components.
+		/// </summary>
+		private static IEnumerable<Assembly> GetAssembliesToGetComponentsFrom()
+			=> AppDomain
+				.CurrentDomain
+				.BaseDirectory
+				.To(directoryPath => Directory.GetFiles(directoryPath, "Postgres.Marula.*.dll"))
+				.Select(AssemblyName.GetAssemblyName)
+				.Select(Assembly.Load);
 
 		/// <summary>
 		/// Predicate function for solution component types filtering.
