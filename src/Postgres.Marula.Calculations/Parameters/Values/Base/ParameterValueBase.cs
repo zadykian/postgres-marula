b5ed181b7174bb5d9570db8c5651f0e3e669c90d
zadykian <zadykian@outlook.com>
2021-02-13 20:06:14 +0300

    ParameterValueBase equality members

--- src/Postgres.Marula.Calculations/Parameters/Values/Base/ParameterValueBase.cs
+++ src/Postgres.Marula.Calculations/Parameters/Values/Base/ParameterValueBase.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Collections.Generic;
 using Postgres.Marula.Calculations.Parameters.Base;
 using Postgres.Marula.Calculations.Parameters.Properties;
 
 namespace Postgres.Marula.Calculations.Parameters.Values.Base
 {
-	/// <inheritdoc />
-	public abstract class ParameterValueBase<T> : IParameterValue
+	/// <inheritdoc cref="IParameterValue"/>
+	public abstract class ParameterValueBase<T> : IParameterValue, IEquatable<ParameterValueBase<T>>
 		where T : notnull
 	{
 		private protected ParameterValueBase(IParameterLink parameterLink, T value)
@@ -26,5 +28,37 @@ namespace Postgres.Marula.Calculations.Parameters.Values.Base
 
 		/// <inheritdoc />
 		public virtual string AsStringValue() => $"{Value.ToString()}{Unit.AsString()}";
+
+		#region EqualityMembers
+
+		/// <inheritdoc />
+		public bool Equals(ParameterValueBase<T>? other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return EqualityComparer<T>.Default.Equals(Value, other.Value) && ParameterLink.Equals(other.ParameterLink) && Unit == other.Unit;
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object? obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
+			return Equals((ParameterValueBase<T>) obj);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode() => HashCode.Combine(Value, ParameterLink, (int) Unit);
+
+		/// <summary>
+		/// <see cref="ParameterValueBase{T}"/> equality operator. 
+		/// </summary>
+		public static bool operator ==(ParameterValueBase<T>? left, ParameterValueBase<T>? right) => Equals(left, right);
+
+		/// <see cref="op_Equality"/>
+		public static bool operator !=(ParameterValueBase<T>? left, ParameterValueBase<T>? right) => !Equals(left, right);
+
+		#endregion
 	}
 }
