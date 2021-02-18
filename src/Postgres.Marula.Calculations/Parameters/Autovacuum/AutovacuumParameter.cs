7d54345526be955e6f193029e39c9d48b4a335a5
zadykian <zadykian@outlook.com>
2021-02-18 22:08:46 +0300

    AutovacuumParameter; TrackCountsParameter

--- src/Postgres.Marula.Calculations/Parameters/Autovacuum/AutovacuumParameter.cs
+++ src/Postgres.Marula.Calculations/Parameters/Autovacuum/AutovacuumParameter.cs
@@ -1,7 +1,5 @@
-using Postgres.Marula.Calculations.ExternalDependencies;
 using Postgres.Marula.Calculations.Parameters.Base;
 using Postgres.Marula.Calculations.ParameterValues;
-using Postgres.Marula.Calculations.ParameterValues.Base;
 using Postgres.Marula.Infrastructure.TypeDecorators;
 
 // ReSharper disable UnusedType.Global
@@ -11,16 +9,12 @@ namespace Postgres.Marula.Calculations.Parameters.Autovacuum
 	/// <summary>
 	/// Controls whether the server should run the autovacuum launcher daemon.
 	/// </summary>
-	internal class AutovacuumParameter : ParameterBase
+	internal class AutovacuumParameter : ParameterBase<BooleanParameterValue, bool>
 	{
-		public AutovacuumParameter(IDatabaseServer databaseServer) : base(databaseServer)
-		{
-		}
-
 		/// <inheritdoc />
 		public override NonEmptyString Name => "autovacuum";
 
 		/// <inheritdoc />
-		public override IParameterValue Calculate() => new BooleanParameterValue(this.GetLink(), value: true);
+		protected override bool CalculateValue() => true;
 	}
 }
--- src/Postgres.Marula.Calculations/Parameters/Statistics/TrackCountsParameter.cs
+++ src/Postgres.Marula.Calculations/Parameters/Statistics/TrackCountsParameter.cs
@@ -1,7 +1,5 @@
-using Postgres.Marula.Calculations.ExternalDependencies;
 using Postgres.Marula.Calculations.Parameters.Base;
 using Postgres.Marula.Calculations.ParameterValues;
-using Postgres.Marula.Calculations.ParameterValues.Base;
 using Postgres.Marula.Infrastructure.TypeDecorators;
 
 // ReSharper disable UnusedType.Global
@@ -12,16 +10,12 @@ namespace Postgres.Marula.Calculations.Parameters.Statistics
 	/// Enables collection of statistics on database activity.
 	/// This parameter affects autovacuum, because the autovacuum daemon needs the collected information.
 	/// </summary>
-	internal class TrackCountsParameter : ParameterBase
+	internal class TrackCountsParameter : ParameterBase<BooleanParameterValue, bool>
 	{
-		public TrackCountsParameter(IDatabaseServer databaseServer) : base(databaseServer)
-		{
-		}
-
 		/// <inheritdoc />
 		public override NonEmptyString Name => "track_counts";
 
 		/// <inheritdoc />
-		public override IParameterValue Calculate() => new BooleanParameterValue(this.GetLink(), value: true);
+		protected override bool CalculateValue() => true;
 	}
 }
