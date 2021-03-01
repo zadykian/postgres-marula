8ba2d458b9d7452330dd8224a28c8ece82d03b15
zadykian <zadykian@outlook.com>
2021-03-01 23:38:49 +0300

    unit column moved to values_history table

--- src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/04-calculated-parameters-table.sql
+++ src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/04-calculated-parameters-table.sql
@@ -5,11 +5,9 @@ set search_path to SYSTEM_SCHEMA_NAME_TO_REPLACE;
 create table PARAMETERS_TABLE_NAME_TO_REPLACE
 (
 	id   smallserial      primary key,
-	name non_empty_string check ( parameter_exists(name) ) unique,
-	unit parameter_unit   not null
+	name non_empty_string check ( parameter_exists(name) ) unique
 );
 
 comment on table  PARAMETERS_TABLE_NAME_TO_REPLACE      is 'Dictionary of parameters calculated by marula.';
 comment on column PARAMETERS_TABLE_NAME_TO_REPLACE.id   is 'Table identifier.';
 comment on column PARAMETERS_TABLE_NAME_TO_REPLACE.name is 'Parameter name. Matches [pg_catalog.pg_settings.name] value.';
-comment on column PARAMETERS_TABLE_NAME_TO_REPLACE.unit is 'Parameter unit.';
--- src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/06-parameter-values-history-table.sql
+++ src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/06-parameter-values-history-table.sql
@@ -7,6 +7,7 @@ create table VALUES_HISTORY_TABLE_NAME_TO_REPLACE
 	id                    bigserial                   primary key,
 	parameter_id          smallint                    not null references calculated_parameters(id),
 	calculated_value      non_empty_string            not null,
+	unit			      parameter_unit              not null,
 	status                STATUS_ENUM_NAME_TO_REPLACE not null,
 	calculation_date_time timestamp                   not null default now()
 );
@@ -15,5 +16,6 @@ comment on table  VALUES_HISTORY_TABLE_NAME_TO_REPLACE                       is 
 comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.id                    is 'Table identifier.';
 comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.parameter_id          is 'Calculated parameter id.';
 comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.calculated_value      is 'Calculated value.';
+comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.unit                  is 'Parameter unit.';
 comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.status                is 'Value calculation status.';
 comment on column VALUES_HISTORY_TABLE_NAME_TO_REPLACE.calculation_date_time is 'Calculation date and time.';
