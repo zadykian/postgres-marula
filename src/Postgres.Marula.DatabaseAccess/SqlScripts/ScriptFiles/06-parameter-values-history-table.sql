7fafacd74f82b1dcdaa7fc6786c8989c78276e6b
zadykian <zadykian@outlook.com>
2021-03-05 17:33:09 +0300

    scripts: identity instead of serial in system tables

--- src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/06-parameter-values-history-table.sql
+++ src/Postgres.Marula.DatabaseAccess/SqlScripts/ScriptFiles/06-parameter-values-history-table.sql
@@ -4,12 +4,12 @@ set search_path to SYSTEM_SCHEMA_NAME_TO_REPLACE;
 
 create table VALUES_HISTORY_TABLE_NAME_TO_REPLACE
 (
-	id                    bigserial                   primary key,
+	id                    bigint                   	  primary key generated always as identity,
 	parameter_id          smallint                    not null references calculated_parameters(id),
 	calculated_value      non_empty_string,
 	unit			      UNIT_ENUM_NAME_TO_REPLACE   not null,
 	status                STATUS_ENUM_NAME_TO_REPLACE not null,
-	calculation_date_time timestamp                   not null default now()
+	calculation_date_time timestamp                   not null default current_timestamp
 );
 
 comment on table  VALUES_HISTORY_TABLE_NAME_TO_REPLACE                       is 'Parameter values calculated my marula service.';
