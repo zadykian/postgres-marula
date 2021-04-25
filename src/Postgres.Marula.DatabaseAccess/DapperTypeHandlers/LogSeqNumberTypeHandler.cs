b4bdfdfbd2bb656dcaa9edeaa24f3c914a00ab96
zadykian <zadykian@outlook.com>
2021-04-25 21:44:55 +0300

    StringLikeTypeHandlerBase

--- src/Postgres.Marula.DatabaseAccess/DapperTypeHandlers/LogSeqNumberTypeHandler.cs
+++ src/Postgres.Marula.DatabaseAccess/DapperTypeHandlers/LogSeqNumberTypeHandler.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Data;
-using Dapper;
+using Postgres.Marula.DatabaseAccess.DapperTypeHandlers.Base;
 using Postgres.Marula.Infrastructure.TypeDecorators;
 
 namespace Postgres.Marula.DatabaseAccess.DapperTypeHandlers
@@ -8,20 +7,9 @@ namespace Postgres.Marula.DatabaseAccess.DapperTypeHandlers
 	/// <summary>
 	/// <see cref="LogSeqNumber"/> dapper type handler.
 	/// </summary>
-	internal class LogSeqNumberTypeHandler : SqlMapper.TypeHandler<LogSeqNumber>
+	internal class LogSeqNumberTypeHandler : StringLikeTypeHandlerBase<LogSeqNumber>
 	{
 		/// <inheritdoc />
-		public override void SetValue(IDbDataParameter parameter, LogSeqNumber value)
-		{
-			parameter.DbType = DbType.String;
-			parameter.Value = value.ToString();
-		}
-
-		/// <inheritdoc />
-		public override LogSeqNumber Parse(object value)
-		{
-			var stringValue = value.ToString() ?? throw new ArgumentNullException();
-			return new LogSeqNumber(stringValue);
-		}
+		public override LogSeqNumber Parse(object value) => new(value.ToString() ?? throw new ArgumentNullException());
 	}
 }
