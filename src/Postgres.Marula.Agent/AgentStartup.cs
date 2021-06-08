a1f671aba71f96896846338455870315ed971b02
zadykian <zadykian@outlook.com>
2021-06-08 12:27:46 +0300

    AgentStartup

---
+++ src/Postgres.Marula.Agent/AgentStartup.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Postgres.Marula.Agent
+{
+	/// <inheritdoc />
+	internal class AgentStartup : StartupBase
+	{
+		/// <inheritdoc />
+		public override void Configure(IApplicationBuilder app)
+		{
+			// todo
+		}
+	}
+}
