81ceb13826b22483fa231cb85ba808b918c002b9
zadykian <zadykian@outlook.com>
2021-06-08 17:36:12 +0300

    HwInfoAppComponent based on current operating system

--- src/Postgres.Marula.HwInfo/HwInfoAppComponent.cs
+++ src/Postgres.Marula.HwInfo/HwInfoAppComponent.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
 using Postgres.Marula.Infrastructure.AppComponents;
+
+// ReSharper disable SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
 
 namespace Postgres.Marula.HwInfo
 {
@@ -8,7 +11,20 @@ namespace Postgres.Marula.HwInfo
 	{
 		/// <inheritdoc />
 		void IAppComponent.RegisterServices(IServiceCollection serviceCollection)
-			=> serviceCollection
-				.AddSingleton<IHardwareInfo, BashHardwareInfo>();
+			=> _ = Environment.OSVersion.Platform switch
+			{
+				PlatformID.Unix    => Use<BashHardwareInfo>(serviceCollection),
+				PlatformID.Win32NT => Use<PowershellHardwareInfo>(serviceCollection),
+				_ => throw new ApplicationException(
+					$"Operating system '{Environment.OSVersion.VersionString}' is not supported.")
+			};
+
+		/// <summary>
+		/// Use type <typeparamref name="THardwareInfo"/>
+		/// as implementation of <see cref="IHardwareInfo"/> interface.
+		/// </summary>
+		private static IServiceCollection Use<THardwareInfo>(IServiceCollection serviceCollection)
+			where THardwareInfo : class, IHardwareInfo
+			=> serviceCollection.AddSingleton<IHardwareInfo, THardwareInfo>();
 	}
 }
