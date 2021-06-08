8f0d8f050629c334a6a6f0a6d6e01d8c8683dfaf
zadykian <zadykian@outlook.com>
2021-06-08 14:47:34 +0300

    swagger

--- src/Postgres.Marula.Agent/HardwareInfoController.cs
+++ src/Postgres.Marula.Agent/HardwareInfoController.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Postgres.Marula.HwInfo;
 using static System.Net.Mime.MediaTypeNames;
@@ -8,7 +9,10 @@ namespace Postgres.Marula.Agent
 	/// <summary>
 	/// Hardware info access.
 	/// </summary>
+	[ApiController]
+	[Route("[controller]/[action]")]
 	[Produces(Application.Json)]
+	[ProducesResponseType(StatusCodes.Status200OK)]
 	public class HardwareInfoController : Controller
 	{
 		private readonly IHardwareInfo hardwareInfo;
@@ -19,12 +23,12 @@ namespace Postgres.Marula.Agent
 		/// Get total size of available RAM.
 		/// </summary>
 		[HttpGet]
-		public async Task<IActionResult> GetTotalRam() => Ok(await hardwareInfo.TotalRam());
+		public async Task<IActionResult> GetTotalRamAsync() => Ok(await hardwareInfo.TotalRam());
 
 		/// <summary>
 		/// Get number of CPU cores.
 		/// </summary>
 		[HttpGet]
-		public async Task<IActionResult> GetCpuCoresCount() => Ok(await hardwareInfo.CpuCoresCount());
+		public async Task<IActionResult> GetCpuCoresCountAsync() => Ok(await hardwareInfo.CpuCoresCount());
 	}
 }
