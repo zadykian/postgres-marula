0d7995ab36c708a9c535eeca81a448c931c92b0b
zadykian <zadykian@outlook.com>
2021-06-10 00:50:15 +0300

    MaxLocksPerTransaction

---
+++ src/Postgres.Marula.Calculations/Parameters/LockManagement/MaxLocksPerTransaction.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Postgres.Marula.Calculations.Parameters.Base;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable BuiltInTypeReferenceStyle
+using LocksCount = System.UInt32;
+
+namespace Postgres.Marula.Calculations.Parameters.LockManagement
+{
+	/// <summary>
+	/// [max_locks_per_transaction]
+	/// The shared lock table tracks locks on
+	/// max_locks_per_transaction * (max_connections + max_prepared_transactions) objects.
+	/// This parameter controls the average number of object locks allocated for each transaction.
+	/// </summary>
+	internal class MaxLocksPerTransaction : IntegerParameterBase
+	{
+		public MaxLocksPerTransaction(ILogger<MaxLocksPerTransaction> logger) : base(logger)
+		{
+		}
+
+		/// <inheritdoc />
+		protected override async ValueTask<LocksCount> CalculateValueAsync() => throw new System.NotImplementedException();
+	}
+}
