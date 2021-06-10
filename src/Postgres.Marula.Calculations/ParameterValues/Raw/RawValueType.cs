8018691f3cc10b4e38ea499dfdc485879e7e9832
zadykian <zadykian@outlook.com>
2021-06-10 23:00:07 +0300

    RawValueType

--- src/Postgres.Marula.Calculations/ParameterValues/Raw/RawValueType.cs
+++ src/Postgres.Marula.Calculations/ParameterValues/Raw/RawValueType.cs
@@ -1,15 +1,36 @@
 namespace Postgres.Marula.Calculations.ParameterValues.Raw
 {
+	/// <summary>
+	/// Type of raw parameter value.
+	/// </summary>
+	/// <remarks>
+	/// Type is being retrieved from 'pg_catalog.pg_settings.vartype' column.
+	/// </remarks>
 	public enum RawValueType : byte
 	{
+		/// <summary>
+		/// Boolean type.
+		/// </summary>
 		Bool,
 
+		/// <summary>
+		/// Enumeration type.
+		/// </summary>
 		Enum,
 
+		/// <summary>
+		/// Integer type.
+		/// </summary>
 		Integer,
 
+		/// <summary>
+		/// Floating point numeric type.
+		/// </summary>
 		Real,
 
+		/// <summary>
+		/// String type.
+		/// </summary>
 		String
 	}
 }
