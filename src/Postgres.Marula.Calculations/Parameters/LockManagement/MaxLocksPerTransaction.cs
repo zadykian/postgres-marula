8132ab2479a2b555647f9af0cab042a437fa523c
zadykian <zadykian@outlook.com>
2021-06-10 14:28:26 +0300

    MaxLocksPerTransaction

--- src/Postgres.Marula.Calculations/Parameters/LockManagement/MaxLocksPerTransaction.cs
+++ src/Postgres.Marula.Calculations/Parameters/LockManagement/MaxLocksPerTransaction.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Postgres.Marula.Calculations.ExternalDependencies;
 using Postgres.Marula.Calculations.Parameters.Base;
+using Postgres.Marula.Infrastructure.Extensions;
 
 // ReSharper disable UnusedType.Global
 // ReSharper disable BuiltInTypeReferenceStyle
 using LocksCount = System.UInt32;
+using Table = Postgres.Marula.Infrastructure.TypeDecorators.SchemaQualifiedObjectName;
 
 namespace Postgres.Marula.Calculations.Parameters.LockManagement
 {
@@ -32,14 +35,25 @@ namespace Postgres.Marula.Calculations.Parameters.LockManagement
 			return (LocksCount) 1.2 * maxPartitionsCount;
 		}
 
+		/// <summary>
+		/// Get maximum count of child partitions among all partitioned and inherited tables.
+		/// Sub-partitions are also taken into account.
+		/// </summary>
 		private async Task<uint> MaxPartitionsCount()
 		{
-			await Task.CompletedTask;
-			
-			var res = databaseServer
-				.GetAllHierarchicalLinks()
-				.GroupBy(parentToChild => parentToChild.Parent)
-				.MaxAsync()
+			var allLinks = await databaseServer.GetAllHierarchicalLinks().ToArrayAsync();
+			if (!allLinks.Any()) return default;
+
+			IEnumerable<Table> AllInheritorsOf(Table parent)
+				=> allLinks!
+					.Where(link => link.Parent.Equals(parent))
+					.Select(link => link.Child)
+					.SelectMany(child => AllInheritorsOf(child).Add(child));
+
+			return (uint) allLinks
+				.Select(link => link.Parent)
+				.Select(parent => AllInheritorsOf(parent).Count())
+				.Max();
 		}
 	}
 }
