430f9ddc014df40a9ed2d8a6dfa340ce496f2c20
zadykian <zadykian@outlook.com>
2021-06-11 17:17:38 +0300

    RemoteHardwareInfo memory type deserialization

--- src/Postgres.Marula.Calculations/HardwareInfo/RemoteHardwareInfo.cs
+++ src/Postgres.Marula.Calculations/HardwareInfo/RemoteHardwareInfo.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Postgres.Marula.Calculations.Configuration;
 using Postgres.Marula.HwInfo;
@@ -40,7 +42,42 @@ namespace Postgres.Marula.Calculations.HardwareInfo
 		{
 			var httpResponseMessage = await httpClient.SendAsync(new(httpMethod, route));
 			var responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
-			return JsonSerializer.Deserialize<TResponse>(responseBody)!;
+			var options = new JsonSerializerOptions { Converters = { MemoryConverter.Instance } };
+			return JsonSerializer.Deserialize<TResponse>(responseBody, options)!;
+		}
+
+		/// <summary>
+		/// JSON read-only converter for <see cref="Memory"/> type.
+		/// </summary>
+		private sealed class MemoryConverter : JsonConverter<Memory>
+		{
+			private MemoryConverter()
+			{
+			}
+
+			/// <summary>
+			/// Converter instance.
+			/// </summary>
+			public static MemoryConverter Instance { get; } = new();
+
+			/// <inheritdoc />
+			public override Memory Read(
+				ref Utf8JsonReader reader,
+				Type typeToConvert,
+				JsonSerializerOptions options)
+			{
+				reader.Read(); // skip start object: '{'
+				reader.Read(); // skip property name: 'totalBytes'
+				var totalBytesValue = reader.GetUInt64();
+				while (reader.TokenType != JsonTokenType.EndObject) reader.Read();
+				return new(totalBytesValue);
+			}
+
+			/// <inheritdoc />
+			public override void Write(
+				Utf8JsonWriter writer,
+				Memory value,
+				JsonSerializerOptions options) => throw new NotSupportedException();
 		}
 	}
 }
