3592952f644812759b795b67ed79ac56a2b36c57
zadykian <zadykian@outlook.com>
2021-06-15 11:16:20 +0300

    ConfirmationResult; ConfirmationTaskExtensions

--- src/Postgres.Marula.AppControl/UIElements/Messages/IMessageBox.cs
+++ src/Postgres.Marula.AppControl/UIElements/Messages/IMessageBox.cs
@@ -11,9 +11,6 @@ namespace Postgres.Marula.AppControl.UIElements.Messages
 		/// <summary>
 		/// Show message box with <paramref name="title"/> and <paramref name="message"/>. 
 		/// </summary>
-		/// <returns>
-		/// <c>true</c> if user confirmed action, otherwise - <c>false</c>.
-		/// </returns>
-		Task<bool> QueryAsync(NonEmptyString title, NonEmptyString message);
+		Task<ConfirmationResult> QueryAsync(NonEmptyString title, NonEmptyString message);
 	}
 }
--- src/Postgres.Marula.AppControl/UIElements/Messages/TerminalMessageBox.cs
+++ src/Postgres.Marula.AppControl/UIElements/Messages/TerminalMessageBox.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using Postgres.Marula.Infrastructure.TypeDecorators;
 using Terminal.Gui;
@@ -9,39 +8,17 @@ namespace Postgres.Marula.AppControl.UIElements.Messages
 	internal class TerminalMessageBox : IMessageBox
 	{
 		/// <inheritdoc />
-		async Task<bool> IMessageBox.QueryAsync(NonEmptyString title, NonEmptyString message)
+		async Task<ConfirmationResult> IMessageBox.QueryAsync(NonEmptyString title, NonEmptyString message)
 		{
 			await Task.CompletedTask;
+
 			var answerButtonNumber = MessageBox.Query(
 				(string) title,
 				(string) message, "yes", "no");
-			return answerButtonNumber == 0;
-		}
-	}
 
-	/// <summary>
-	/// Extension methods for <see cref="IMessageBox"/> fluent syntax.
-	/// </summary>
-	internal static class MessageBoxExtensions
-	{
-		/// <summary>
-		/// Execute function <paramref name="onConfirmed"/>
-		/// if <paramref name="confirmationTask"/> contains <c>true</c>. 
-		/// </summary>
-		public static async Task OnConfirmed(this Task<bool> confirmationTask, Func<Task> onConfirmed)
-		{
-			var confirmed = await confirmationTask;
-			if (confirmed) await onConfirmed();
-		}
-		
-		/// <summary>
-		/// Execute function <paramref name="onConfirmed"/>
-		/// if <paramref name="confirmationTask"/> contains <c>true</c>. 
-		/// </summary>
-		public static async Task OnConfirmed(this Task<bool> confirmationTask, Func<ValueTask> onConfirmed)
-		{
-			var confirmed = await confirmationTask;
-			if (confirmed) await onConfirmed();
+			return answerButtonNumber == 0
+				? ConfirmationResult.Confirmed
+				: ConfirmationResult.Rejected;
 		}
 	}
 }
