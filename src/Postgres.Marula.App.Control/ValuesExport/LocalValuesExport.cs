52674c7bb426b302447eecce991f93a08cfdc701
zadykian <zadykian@outlook.com>
2021-06-22 01:14:51 +0300

    LocalValuesExport

--- src/Postgres.Marula.App.Control/ValuesExport/LocalValuesExport.cs
+++ src/Postgres.Marula.App.Control/ValuesExport/LocalValuesExport.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Postgres.Marula.Calculations.PublicApi;
 using Postgres.Marula.Infrastructure.TypeDecorators;
@@ -11,8 +14,11 @@ namespace Postgres.Marula.App.Control.ValuesExport
 		/// <inheritdoc />
 		async Task<NonEmptyString> IValuesExport.ExportAsync(IEnumerable<IValueView> valueViews)
 		{
-			await Task.CompletedTask;
-			return "test.sql";
+			var fileName = $"marula-profile-{DateTime.Now:yyyy-MM-dd_hh-mm-ss}.sql";
+			var fullPath = Path.Combine(Environment.CurrentDirectory, fileName);
+			var alterSystemLines = valueViews.Select(view => (string) view.AsAlterSystem());
+			await File.WriteAllLinesAsync(fullPath, alterSystemLines);
+			return fileName;
 		}
 	}
 }
