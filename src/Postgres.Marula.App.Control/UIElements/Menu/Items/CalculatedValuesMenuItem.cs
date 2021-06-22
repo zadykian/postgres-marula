5ac5d3eb9dd62df6ac8ea9fef17d35a0fd45b6d2
zadykian <zadykian@outlook.com>
2021-06-22 09:04:50 +0300

    CalculatedValuesMenuItem

--- src/Postgres.Marula.App.Control/UIElements/Menu/Items/CalculatedValuesMenuItem.cs
+++ src/Postgres.Marula.App.Control/UIElements/Menu/Items/CalculatedValuesMenuItem.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Postgres.Marula.App.Control.UIElements.MainViews;
+using Postgres.Marula.App.Control.UIElements.Messages;
 using Postgres.Marula.Calculations.PublicApi;
 
 namespace Postgres.Marula.App.Control.UIElements.Menu.Items
@@ -12,13 +13,16 @@ namespace Postgres.Marula.App.Control.UIElements.Menu.Items
 	{
 		private readonly IParameterValues parameterValues;
 		private readonly IOutputWindow outputWindow;
-		
+		private readonly IMessageBox messageBox;
+
 		public CalculatedValuesMenuItem(
 			IParameterValues parameterValues,
-			IOutputWindow outputWindow) : base("view calculated values", 1)
+			IOutputWindow outputWindow,
+			IMessageBox messageBox) : base("view calculated values", 1)
 		{
 			this.parameterValues = parameterValues;
 			this.outputWindow = outputWindow;
+			this.messageBox = messageBox;
 		}
 
 		/// <inheritdoc />
@@ -33,6 +37,13 @@ namespace Postgres.Marula.App.Control.UIElements.Menu.Items
 			outputWindow
 				.Titled("calculated values")
 				.Show(valueViews);
+
+			if (!valueViews.Any())
+			{
+				messageBox.Show(
+					"unable to load parameter values",
+					"there are no any calculated values found!");
+			}
 		}
 	}
 }
